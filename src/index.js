77ce86c9d8918961acc45304dc05e678a816474a
Mayara Marques <mayaramarquescosta@outlook.com>
2022-11-04 17:40:55 -0300

    Requisito 2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
 const path = require('path');
+// const { readFile } = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -24,4 +25,13 @@ app.get('/talker', async (_req, res) => {
   const talkers = await fs.readFile(tPath, 'utf-8');
   const info = talkers ? JSON.parse(talkers) : [];
   return res.status(200).json(info);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await fs.readFile(tPath, 'utf-8');
+  const data = JSON.parse(talkers);
+  const getId = data.find((el) => el.id === Number(id));
+  if (!getId) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  return res.status(200).json(getId);
 });
