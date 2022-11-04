21bd1fdb7e5f8f14a9118e9896a6b99e1a1d8f40
Mayara Marques <mayaramarquescosta@outlook.com>
2022-11-04 16:27:16 -0300

    Requisito 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-//Iniciando o Projeto
-
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,4 +16,12 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+const tPath = path.resolve(__dirname, './talker.json');
+
+app.get('/talker', async (_req, res) => {
+  const talkers = await fs.readFile(tPath, 'utf-8');
+  const info = talkers ? JSON.parse(talkers) : [];
+  return res.status(200).json(info);
 });
